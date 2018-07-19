f94ee026b53ef063c25a582b30d4f97563c14550
eric <eyea@qq.com>
2018-07-20 00:37:03 +0800

    format

--- tempConvert2.py
+++ tempConvert2.py
@@ -1,7 +1,7 @@
-tempStr=input()
-if tempStr[0] in ['C','c']:
-    f=eval(tempStr[1:])*1.8+32
+tempStr = input()
+if tempStr[0] in ['C', 'c']:
+    f = eval(tempStr[1:])*1.8+32
     print("F{:.2f}".format(f))
-elif tempStr[0] in ['F','f']:
-    c=(eval(tempStr[1:])-32)/1.8
+elif tempStr[0] in ['F', 'f']:
+    c = (eval(tempStr[1:])-32)/1.8
     print("C{:.2f}".format(c))
